e14e1fb094e3cd486ad7fd469fca632bffbf38fb
raeubert <robertschulz@tutanota.com>
2020-10-13 15:40:58 +0200

    new

--- src/server/Main.java
+++ src/server/Main.java
@@ -3,6 +3,6 @@ package server;
 public class Main {
     public static void main(String[] args) {
         TCPServer server1 = new TCPServer();
-        server1.TCPServer();
+        server1.server();
     }
 }
